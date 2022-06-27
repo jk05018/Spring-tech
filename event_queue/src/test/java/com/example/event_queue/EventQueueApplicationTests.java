80e9c9352d757e56b1753e6e47de1393305cebe8
SeungHan <jk05018@naver.com>
2022-06-27 16:20:59 +0900

    build : event queue project build

---
+++ event_queue/src/test/java/com/example/event_queue/EventQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
