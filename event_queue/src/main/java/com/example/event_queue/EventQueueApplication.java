01bec46be1513f0b9da555e116e99a02ed724cd4
SeungHan <jk05018@naver.com>
2022-06-28 18:09:36 +0900

    feat : objectMapper 설정 추가

--- event_queue/src/main/java/com/example/event_queue/EventQueueApplication.java
+++ event_queue/src/main/java/com/example/event_queue/EventQueueApplication.java
@@ -1,13 +1,29 @@
 package com.example.event_queue;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventQueueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventQueueApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, false);
+		return objectMapper;
 	}
 
 }
