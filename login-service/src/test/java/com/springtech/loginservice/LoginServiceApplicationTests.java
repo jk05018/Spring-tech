7b1ecf84c9933b74a25250bc40847d4ede56e2ee
SeungHan <jk05018@naver.com>
2022-07-13 17:15:00 +0900

    build : login service project build

---
+++ login-service/src/test/java/com/springtech/loginservice/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtech.loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
