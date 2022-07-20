11211841049be95c255d6db9b4b7020a503f0a18
SeungHan <jk05018@naver.com>
2022-07-20 22:55:52 +0900

    [LOGIN-8] Github 로그인 개발

--- login-service/src/main/java/com/springtech/loginservice/config/auth/OAuthAttributes.java
+++ login-service/src/main/java/com/springtech/loginservice/config/auth/OAuthAttributes.java
@@ -27,26 +27,40 @@ public class OAuthAttributes {
 
   public static OAuthAttributes of(String registrationId, String userNameAttributeName,
       Map<String, Object> attributes) {
+    if ("github".equals(registrationId)) {
+      return ofGithub("login", attributes);
+    }
     if ("kakao".equals(registrationId)) {
-      return ofKakao("id", attributes);
+      return ofKakao("nickname", attributes);
     }
     if ("naver".equals(registrationId)) {
-      return ofNaver("id", attributes);
+      return ofNaver("name", attributes);
     }
     return ofGoogle(userNameAttributeName, attributes);
   }
 
   private static OAuthAttributes ofKakao(String userNameAttributeName,
       Map<String, Object> attributes) {
-    // kakao는 kakao_account에 유저정보가 있다. (email)
     Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-    // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
     Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
 
     return OAuthAttributes.builder()
         .name((String) kakaoProfile.get("nickname"))
         .email((String) kakaoAccount.get("email"))
         .picture((String) kakaoProfile.get("profile_image_url"))
+        .attributes(attributes)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
+
+  private static OAuthAttributes ofGithub(String userNameAttributeName,
+      Map<String, Object> attributes) {
+
+    System.out.println("email is " + (String) attributes.get("email"));
+    return OAuthAttributes.builder()
+        .name((String) attributes.get("login"))
+        .email((String) attributes.get("email"))
+        .picture((String) attributes.get("avatar_url"))
         .attributes(attributes)
         .nameAttributeKey(userNameAttributeName)
         .build();
@@ -70,7 +84,7 @@ public class OAuthAttributes {
     return OAuthAttributes.builder()
         .name((String) response.get("name"))
         .email((String) response.get("email"))
-        .picture((String) response.get("picture"))
+        .picture((String) response.get("profileImage"))
         .attributes(response)
         .nameAttributeKey(userNameAttributeName)
         .build();
